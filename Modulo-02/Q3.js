c5ee6a9d6602b3dcd6f23ebcd6bfb4c7a771e880
Larissa Nascimento <113188584+Larissa022@users.noreply.github.com>
2026-01-20 11:25:51 -0300

    Create Q3.js

---
+++ Modulo-02/Q3.js
@@ -0,0 +1,14 @@
+class Fruta {
+constructor(nome, cor, peso){
+this.nome = nome;
+this.cor = cor;
+this.peso = peso;
+}
+}
+let maçã = new Fruta("maçã", "vermelha", "150 kg");
+let manga = new Fruta("manga", "vermelho e laranja", "300g");
+
+let melancia = new Fruta("melancia", "verde", "1.5kg");
+console.log(maçã);
+console.log(manga);
+console.log(melancia);
