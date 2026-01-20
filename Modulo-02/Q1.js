7775620d716ac1a8579de75416442c346100d429
Larissa Nascimento <113188584+Larissa022@users.noreply.github.com>
2026-01-20 11:24:21 -0300

    Create Q1.js

---
+++ Modulo-02/Q1.js
@@ -0,0 +1,10 @@
+class Pessoa {
+constructor(nome, idade){
+this.nome = nome;
+this.idade = idade;
+}
+}
+let pessoa1 = new Pessoa("Rafael", 20);
+let pessoa2 = new Pessoa("Vitória", 15);
+console.log(pessoa1.nome);
+console.log(pessoa2.idade);
